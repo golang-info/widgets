6639ea9d0f042e7adc26a79b2e967a6fa47c90f9
ycliu912 <ycliu912@126.com>
2020-09-15 13:23:39 +0800

    add Widget interface

--- widgets.go
+++ widgets.go
@@ -2,11 +2,17 @@ package widgets
 
 import uuid "github.com/satori/go.uuid"
 
+// Widget is a ...
+type Widget interface {
+	// ID 返回这个 widget 的唯一标识符
+	ID() string
+}
+
 type widget struct {
 	id string
 }
 
-func NewWidget() widget {
+func NewWidget() Widget {
 	return widget{
 		id: uuid.NewV4().String(),
 	}
