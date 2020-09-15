6af28229ba56ac11ab54362a6cbd0a6a664518a1
ycliu912 <ycliu912@126.com>
2020-09-15 10:58:36 +0800

    add NewWidget

--- widgets.go
+++ widgets.go
@@ -1,10 +1,17 @@
 package widgets
 
+import uuid "github.com/satori/go.uuid"
+
 type Widget struct {
 	id string
+}
+
+func NewWidget() Widget {
+	return Widget{
+		id: uuid.NewV4().String(),
+	}
 }
 
 func (w Widget) ID() string {
 	return w.id
 }
-
